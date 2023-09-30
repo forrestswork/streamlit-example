6db170cb998e73cc09fe13360fa957d9fef9ad85
Forrest Lyman <45180463+forrestswork@users.noreply.github.com>
2023-09-29 20:34:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,6 @@ import streamlit as st
 
 """
 # This is my test!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
